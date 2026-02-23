647d177fc74842b12c2e9a1da8f64681d7acb4b9
prasannamayil <prasanna.mayilvahanan@gmail.com>
2026-02-23 17:07:20 +0100

    fixing grad accum

--- dllm/examples/gsm_infinity/run_pretrain_400M.sh
+++ dllm/examples/gsm_infinity/run_pretrain_400M.sh
@@ -141,8 +141,8 @@ train_mdlm() {
         --lr_scheduler_type cosine \
         --warmup_ratio 0.05 \
         --max_grad_norm 1.0 \
-        --per_device_train_batch_size 16 \
-        --gradient_accumulation_steps 4 \
+        --per_device_train_batch_size 64 \
+        --gradient_accumulation_steps 1 \
         --bf16 True \
         --gradient_checkpointing True \
         --logging_steps 10 \
@@ -188,8 +188,8 @@ train_bd3lm() {
         --lr_scheduler_type cosine \
         --warmup_ratio 0.05 \
         --max_grad_norm 1.0 \
-        --per_device_train_batch_size 8 \
-        --gradient_accumulation_steps 8 \
+        --per_device_train_batch_size 64 \
+        --gradient_accumulation_steps 1 \
         --block_size 32 \
         --attn_implementation flex_attention \
         --bf16 True \
